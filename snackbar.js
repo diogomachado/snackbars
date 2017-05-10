a91a1541bdc3135ff11a6509a3a496f13c7d1137
Diogo Machado <diogomachado@saocamilo-es.br>
2017-05-10 10:51:19 -0300

    Implementacao inicial de suporte a botoes de acao

--- snackbar.js
+++ snackbar.js
@@ -2,22 +2,37 @@ var snackbar = (function(){
 
     var obj = {};
 
-    obj.show = function(message){
+    obj.show = function(message, button){
+
         var div = document.getElementsByClassName('snackbar');
+        var text = document.querySelector('.snackbar p');
+
+        if (button){
+            var buttonNew = document.createElement('button');
+            buttonNew.id = button.id;
+            buttonNew.innerText = button.title;
+            div[0].appendChild(buttonNew);
+        }
+
         div[0].className = "snackbar";
         div[0].className += " show";
-        div[0].innerHTML = message;
+        text.innerHTML = message;
     };
 
     obj.hide = function(){
         var div = document.getElementsByClassName('snackbar');
+        var text = document.querySelector('.snackbar p');
         div[0].className = "snackbar hide";
-        div[0].innerHTML = "";
+        text.innerHTML = "";
     };
 
-    obj.timer = function(message, miliseconds){
+    obj.timer = function(message, miliseconds, button){
 
-        obj.show(message);
+        var button = {};
+        button.title = "Voltar";
+        button.id = "btn_voltar";
+
+        obj.show(message, button);
 
         setTimeout(function() {
             obj.hide();
