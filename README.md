d480a0efec550b3efdd6f05250476d384b69daeb
Diogo Machado <diogomachado@saocamilo-es.br>
2017-05-23 17:24:03 -0300

    New support button action

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ Message after a certain time:
 ``` js
 snackbar.timer("Hello", 3000);
 ```
+
+Message with button:
+``` js
+// Create new button
+var button = {};
+button.id = "btn_cancel";
+button.title = "Cancel";
+
+// Set function callback
+snackbar.setCallback(test);
+
+// Call
+snackbar.timer("Hello World", 3000, button);
+```
--- snackbar.js
+++ snackbar.js
@@ -2,40 +2,62 @@ var snackbar = (function(){
 
     var obj = {};
 
+    // Reference to the name of a function that will be executed by the button
+    obj.setCallback = function(callbackFunction){
+        obj.callback = callbackFunction;
+    }
+
     obj.show = function(message, button){
 
         var div = document.getElementsByClassName('snackbar');
         var text = document.querySelector('.snackbar p');
 
+        // If you have a button set
         if (button){
             var buttonNew = document.createElement('button');
+
+            // Button properties
             buttonNew.id = button.id;
             buttonNew.innerText = button.title;
+            buttonNew.addEventListener('click', obj.callback);
+
             div[0].appendChild(buttonNew);
         }
 
-        div[0].className = "snackbar";
+        // Places a size class according to the size of the string
+        if (message.length > 28){
+            if (message.length <= 35){
+                div[0].className += " snackbar-md";
+            }else{
+                div[0].className += " snackbar-lg";
+            }
+        }
+
         div[0].className += " show";
         text.innerHTML = message;
     };
 
-    obj.hide = function(){
+    obj.hide = function(time){
+
         var div = document.getElementsByClassName('snackbar');
         var text = document.querySelector('.snackbar p');
-        div[0].className = "snackbar hide";
-        text.innerHTML = "";
+        div[0].className += " hide";
+
+        setTimeout(function(){
+            div[0].className = "snackbar";
+            text.innerHTML = "";
+            div[0].innerHTML = "<p></p>";
+        }, 300);
     };
 
     obj.timer = function(message, miliseconds, button){
 
-        var button = {};
-        button.title = "Voltar";
-        button.id = "btn_voltar";
+        var div = document.getElementsByClassName('snackbar');
 
         obj.show(message, button);
 
         setTimeout(function() {
-            obj.hide();
+            obj.hide(miliseconds);
         }, miliseconds);
     };
 
